0c6ffd947ebc381e3457c3337c1dab17c6c95448
Kudyakov Artem <t3m8ch@yandex.ru>
2021-06-21 13:32:19 +0400

    feat: Add an option to see karma of replied user

--- karma_bot/handlers/getkarma.py
+++ karma_bot/handlers/getkarma.py
@@ -4,10 +4,10 @@ from aiogram.dispatcher.filters import Text
 from karma_bot.loader import dp, karma_service
 
 
-@dp.message_handler(lambda m: len(m.text.split()) == 2, commands=["karma"])
+@dp.message_handler(lambda m: len(m.text.split()) == 2, commands=["karma"], is_reply=False)
 @dp.message_handler(lambda m: len(m.text.split()) == 2 and (
                               "карма" in m.text or
-                              "karma" in m.text))
+                              "karma" in m.text), is_reply=False)
 async def cmd_karma_of_user(message: types.Message):
     user_info = message.text.split()[1]
     if "@" in user_info:
@@ -20,9 +20,17 @@ async def cmd_karma_of_user(message: types.Message):
                             f"Либо ничего не писать: /karma")
 
 
-@dp.message_handler(lambda m: len(m.text.split()) == 1, commands=["karma"])
-@dp.message_handler(Text(equals=["karma", "карма"]))
+@dp.message_handler(lambda m: len(m.text.split()) == 1, commands=["karma"], is_reply=False)
+@dp.message_handler(Text(equals=["karma", "карма"]), is_reply=False)
 async def cmd_karma_of_self(message: types.Message):
     user_id = message.from_user.id
     karma = await karma_service.get_karma(user_id)
     await message.reply(f"Ваша карма - {karma}")
+
+
+@dp.message_handler(lambda m: len(m.text.split()) == 1, commands=["karma"], is_reply=True)
+@dp.message_handler(Text(equals=["karma", "карма"]), is_reply=True)
+async def cmd_karma_of_replied_user(message: types.Message):
+    user = message.reply_to_message.from_user
+    karma = await karma_service.get_karma(user.id)
+    await message.reply(f"Карма @{user.username} - {karma}")
