675f3a7d2229093a8bb89f7394b25d9c91356be0
Kudyakov Artem <t3m8ch@yandex.ru>
2021-06-21 12:04:33 +0400

    refactor: Change to 'CommandStart()'

--- karma_bot/handlers/common.py
+++ karma_bot/handlers/common.py
@@ -1,8 +1,9 @@
 from aiogram import types
+from aiogram.dispatcher.filters import CommandStart
 
 from karma_bot.loader import dp
 
 
-@dp.message_handler(commands=["start"])
+@dp.message_handler(CommandStart())
 async def cmd_start(message: types.Message):
     await message.reply("Hello!")
